b23cf2e4e3f193c0b910263cd13063931d5829bf
Ankita-swami-tech <ankitathimau2006@gmail.com>
2025-07-01 12:42:33 +0530

    Initial commit (secrets secured)

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 
  export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://ankitathimau2006:ankuvikku@cluster0.73aapqa.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected"));
 
  }
